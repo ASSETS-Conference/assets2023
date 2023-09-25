3d3be20490222a747c7d4d1967673a85aa2fbf5f
oliveralonzo <oliveralonzo@users.noreply.github.com>
2023-09-25 14:55:22 -0500

    Optimize sticky header code for program

--- assets/js/sticky_header.js
+++ assets/js/sticky_header.js
@@ -3,54 +3,82 @@ function set_sticky(query, sticky_class, offset = 0) {
     let scroll = $(window).scrollTop();
     let el_height = $(elements[0]).outerHeight();
 
-    let locations = elements.map(function () {
+    let locations = getStartingLocations(elements);
+
+    let sticky_index = findCurrent(locations, scroll, offset);
+
+    let sticky = toggleSticky(elements, sticky_index, sticky_class);
+    if (sticky != null) {
+        setOffsets(elements, locations, sticky, sticky_index, scroll, offset);
+    }
+    
+    return sticky.outerHeight();
+}
+
+function getStartingLocations(elements) {
+    return elements.map(function () {
         return $(this).offset().top;
     });
+}
 
-    var sticky_index = null;
-
+function findCurrent(locations, curr, offset) {
     for (let index = locations.length - 1; index >= 0; index--) {
-        if (scroll >= locations[index] - offset) {
-            sticky_index = index;     
-            break;
+        if (curr >= locations[index] - offset) {
+            return index;
         }
     };
 
-    $(`.${sticky_class}`).remove();
+    return null;
+}
+
+function toggleSticky(elements, sticky_index, sticky_class) {
+    let sticky = $(`.${sticky_class}`);
 
     if (sticky_index != null) {
-
         let el = $(elements[sticky_index]);
+        let new_sticky_id = `${sticky_class}-${sticky_index}`;
 
-        let sticky = el.clone().addClass(sticky_class);
-        el.parent().prepend(sticky);
-        
-        
-        let left_offset = $(`${query}:not(.${sticky_class})`).offset().left;
-        sticky.css("paddingLeft", left_offset);
+        if (sticky != null) {
+            var curr_sticky_id = $(sticky).attr("id");
+        }
 
-        sticky.css("top", offset);
-        if (sticky_index != locations.length - 1) {
-            let next_index = sticky_index + 1;
-            let next_el = $(elements[next_index])
-            let next_location = locations[next_index];
-            
-            var add_offset = 0;
+        if (curr_sticky_id != new_sticky_id) {
+            sticky.remove();
 
-            console.log(sticky.parent().parent(), next_el.parent().parent())
+            sticky = el.clone();
+            sticky.addClass(sticky_class);
+            sticky.attr("id", new_sticky_id);
+            el.parent().prepend(sticky);
+        }
+    } else {
+        sticky.remove();
+    }
+    
+    return sticky;
+}
 
-            if (sticky.parent().parent().get(0) != next_el.parent().parent().get(0)) {
-                console.log("not the same grandpa");
-                var add_offset = offset;
-            }
+function setOffsets(elements, locations, sticky, sticky_index, scroll, x_offset) {
+    let left_offset = elements.offset().left;
+    let sticky_height = sticky.outerHeight();
+    sticky.css("paddingLeft", left_offset);
 
-            if (scroll + add_offset + offset + el_height > next_location) {
-                sticky.css("top", -(el_height + add_offset + scroll - next_location));
-            } 
+    sticky.css("top", x_offset);
+    if (sticky_index != locations.length - 1) {
+        let next_index = sticky_index + 1;
+        let next_el = $(elements[next_index])
+        let next_location = locations[next_index];
+
+        var add_x_offset = 0;
+
+        if (sticky.parent().parent().get(0) != next_el.parent().parent().get(0)) {
+            var add_x_offset = x_offset;
+        }
+
+        if (scroll + add_x_offset + x_offset + sticky_height > next_location) {
+            x_offset = Math.floor(-(sticky_height + add_x_offset + scroll - next_location));
+            sticky.css("top", x_offset);
         }
     }
-
-    return el_height;
 }
 
 $(window).scroll(function () {
